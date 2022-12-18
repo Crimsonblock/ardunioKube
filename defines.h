ae2def720d8c66b92b9d440663dcfeda5e07c186
Crimsonblock <crimsonblock1991+github@gmail.com>
2022-12-18 17:34:26 +0100

    Switched back to serial communication

--- defines.h
+++ defines.h
@@ -1,31 +1,18 @@
 #ifndef DEFINES_H_
 #define DEFINES_H_
-#include "UUIDs.h"
 #include "defines.h"
 #include <Arduino_LSM9DS1.h>
-#include <ArduinoBLE.h>
 
 
 #define DEVICE_NAME "Kube"
 
-#define SAMPLING_PERIOD 34
+#define SAMPLING_PERIOD 33
 #define DEG_TO_RAD (PI/180)
 
 
 #define IMU_TYPE 0
-#define ROT_TYPE 0
+#define ROT_TYPE 1
 
-BLECharacteristic IMUChar(Y_ACCEL_CHARACTERISTIC, BLERead | BLENotify, 24);
-
-BLEFloatCharacteristic frontChar(ENCODER_CHARACTERISTIC_FRONT, BLERead | BLENotify);
-BLEFloatCharacteristic backChar(ENCODER_CHARACTERISTIC_BACK, BLERead | BLENotify);
-BLEFloatCharacteristic leftChar(ENCODER_CHARACTERISTIC_LEFT, BLERead | BLENotify);
-BLEFloatCharacteristic rightChar(ENCODER_CHARACTERISTIC_RIGHT, BLERead | BLENotify);
-BLEFloatCharacteristic topChar(ENCODER_CHARACTERISTIC_TOP, BLERead | BLENotify);
-BLEFloatCharacteristic bottomChar(ENCODER_CHARACTERISTIC_BOTTOM, BLERead | BLENotify);
-
-
-void initBLE();
 
 
 int A[6] = { 2, 4, 6, 8, 10, 12 };
--- v3.ino
+++ v3.ino
@@ -5,8 +5,6 @@ typedef union {
   char buffer[4];
 } floaty_t;
 
-void sendBleImu(float aX, float aY, float aZ, float gX, float gY, float gZ);
-
 int prev;
 
 
@@ -16,94 +14,34 @@ void setup() {
   Serial.begin(9600);
 
   // Inits IMU
-  if(!IMU.begin()){
-    while(true) Serial.println("Error while init IMU");
+  if (!IMU.begin()) {
+    while (true) Serial.println("Error while init IMU");
   }
 
   // Inits BLE
-  initBLE();
   prev = 0;
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(millis()-prev>=SAMPLING_PERIOD){
+  if (millis() - prev >= SAMPLING_PERIOD) {
     prev = millis();
+    float aX, aY, aZ, gX, gY, gZ;
+    IMU.readGyroscope(gX, gY, gZ);
+    IMU.readAcceleration(aX, aY, aZ);
 
-    BLEDevice central = BLE.central();
-    if(central){
-      float aX, aY, aZ, gX, gY, gZ;
-      IMU.readGyroscope(gX, gY, gZ);
-      IMU.readAcceleration(aX, aY, aZ);
+    gX = gX * DEG_TO_RAD;
+    gY = gY * DEG_TO_RAD;
+    gZ = gZ * DEG_TO_RAD;
 
-      gX = gX * DEG_TO_RAD;
-      gY = gY * DEG_TO_RAD;
-      gZ = gZ * DEG_TO_RAD;
-
-      sendBleImu(aX, aY, aZ, gX, gY, gZ);
-
-      Serial.println((String) "Communication time: " + (millis() - prev) );
-    }
+    sendSerialImu(aX, aY, aZ, gX, gY, gZ);
   }
 }
 
-
-void sendBleImu( float aX, float aY, float aZ, float gX, float gY, float gZ ){
-  floaty_t a;
-  byte buffer[24];
-  a.v = aX;
-
-  // Acceleromeer
-  for(int i=0; i<4 ; i++){
-    buffer[i] = a.buffer[3-i];
-  }
-  a.v = aY;
-  for(int i=0; i<4;i++){
-    buffer[4+i] = a.buffer[3-i];
-  }
-  a.v = aZ;
-  for(int i=0; i<4; i++){
-    buffer[8+i] = a.buffer[3-i];
-  }
-
-  //Gyroscope
-  a.v = gX;
-  for(int i=0; i<4; i++){
-    buffer[12+i] = a.buffer[3-i];
-  }
-  a.v = gY;
-  for(int i=0; i<4; i++){
-    buffer[16+i] = a.buffer[3-i];
-  }
-  a.v = gZ;
-  for(int i=0; i<4; i++){
-    buffer[20+i] = a.buffer[3-i];
-  }
-
-  IMUChar.writeValue(buffer, 24);
-
+void sendSerialImu(float aX, float aY, float aZ, float gX, float gY, float gZ) {
+  char buffer[100];
+  snprintf(buffer, 100, "i%f %f %f %f %f %f", aX, aY, aZ, gX, gY, gZ);
+  Serial.println(buffer);
 }
 
-
-
-void initBLE(){
-  if (!BLE.begin()) {
-    Serial.println("Unable to initialize BLE module");
-    while (1)
-      ;
-  }
-
-
-  BLE.setDeviceName(DEVICE_NAME);
-  BLE.setLocalName(DEVICE_NAME);
-
-  BLEService kubeService(KUBE_SERVICE);
-
-  kubeService.addCharacteristic(IMUChar);
-
-  BLE.addService(kubeService);
-
-  BLE.setConnectable(true);
-  BLE.advertise();
-}
